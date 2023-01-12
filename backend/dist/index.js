82062f953bdf8e43a7ae2364ab7a2b81ded95a93
kaddy120 <kaddy120@gmail.com>
2023-01-12 20:43:47 +0200

    create a site asserts in www/

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,10 +4,47 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
-const port = 8081;
+const port = 8080;
+// async function createSiteConf(domain: string): Promise<void> {
+// 	throw new Error("not yet implemented")
+// }
+function initiateSiteFiles(domain) {
+    let rootPath = path_1.default.resolve(__dirname, "..");
+    rootPath = path_1.default.resolve(rootPath, "..");
+    const publicDir = path_1.default.join(rootPath, "www", domain, "public_html");
+    console.log(publicDir);
+    const logDir = path_1.default.join(rootPath, "www", domain, "logs");
+    console.log(logDir);
+    fs_1.default.mkdir(publicDir, { recursive: true }, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    fs_1.default.mkdir(logDir, { recursive: true }, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    function callback(err) {
+        if (err)
+            throw err;
+        console.log('source.txt was copied to destination.txt');
+    }
+    // destination.txt will be created or overwritten by default.
+    fs_1.default.copyFile(`${rootPath}/index.html`, `${publicDir}/index.html`, callback);
+}
+function a2ensite(filePath) {
+    throw new Error("not yet implemented");
+}
 app.get("/", (req, res) => {
     res.send("Hello world");
+});
+app.get("/create/:domain", (req, res) => {
+    const domain = req.params.domain;
+    initiateSiteFiles(domain);
 });
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,57 @@
-import express, {Request, Response, Express} from "express";
+import express, { Request, Response, Express } from "express";
+import path from "path";
+import fs from "fs"
+
 const app: Express = express();
-const port: number = 8081;
+const port = 8080;
+
+// async function createSiteConf(domain: string): Promise<void> {
+// 	throw new Error("not yet implemented")
+// }
+
+
+function initiateSiteFiles(domain: string) {
+	let rootPath: string = path.resolve(__dirname, "..");
+	rootPath= path.resolve(rootPath, "..");
+	const publicDir: string = path.join(rootPath, "www", domain, "public_html")
+	console.log(publicDir)
+	const logDir: string = path.join(rootPath, "www", domain, "logs")
+	console.log(logDir)
+
+	fs.mkdir(publicDir, { recursive: true }, (err) => {
+		if (err) {
+			throw err;
+		}
+	});
+
+	fs.mkdir(logDir, { recursive: true }, (err) => {
+		if (err) {
+			throw err;
+		}
+	});
+
+
+	function callback(err: NodeJS.ErrnoException | null): void {
+		if (err) throw err;
+		console.log('source.txt was copied to destination.txt');
+	}
+
+	// destination.txt will be created or overwritten by default.
+	fs.copyFile(`${rootPath}/index.html`, `${publicDir}/index.html`, callback);
+}
+
+function a2ensite(filePath: string): void {
+	throw new Error("not yet implemented")
+}
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello world");
 });
+
+app.get("/create/:domain", (req: Request<{ domain: string }>, res: Response) => {
+	const domain: string = req.params.domain;
+	initiateSiteFiles(domain)
+})
 
 app.listen(port, () => {
 	console.log(`server started at http://localhost:${port}`);
